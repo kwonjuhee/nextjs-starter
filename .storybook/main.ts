51e188cd73f9f1559390509ba46d035c119442d9
juhee <77ooee@gmail.com>
2023-12-04 22:14:08 +0900

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, ".."),
+      };
+    }
+
+    return config;
+  },
+};
+export default config;
